0d52add07d3bdd7733e17ba07745a7e9aca8f666
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-14 11:49:33 +0300

    Add files via upload

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render , get_object_or_404
+from .models import Book
+from .models import Library
+from django.views.generic.detail import DetailView
+
+# Class-Based View
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+# Function-Based View
+"relationship_app/list_books.html"
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
