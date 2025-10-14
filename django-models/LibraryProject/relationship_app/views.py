70b30de3d34b01ac0c4ab294700b183b4671e616
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-14 18:32:19 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render , get_object_or_404 , redirect
-from django.contrib.auth import login, logout,authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-from .models import Book ,Library
+from .models import Book 
+from .models import Library
 from django.views.generic.detail import DetailView
-
+from django.contrib.auth.decorators import user_passes_test
 
 # Class-Based View
 class LibraryDetailView(DetailView):
@@ -51,5 +52,19 @@ def logout_view(request):
     logout(request)
     messages.info(request, 'You have been logged out.')
     return redirect('login')
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 
