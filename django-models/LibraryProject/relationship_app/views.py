dd9bc5b82e3e370512db4aa5bd038f9654febb63
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-14 11:11:34 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,6 +9,7 @@ class LibraryDetailView(DetailView):
     template_name = 'library_detail.html'
     context_object_name = 'library'
 # Function-Based View
-def list_books(request):
+def book_list(request):
     books = Book.objects.all()
+
     return render(request, 'book_list.html', {'books': books})
