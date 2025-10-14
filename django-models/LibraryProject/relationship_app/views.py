8db773c2461fa4f125bd8027835f93d980ad2f1c
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-14 13:55:10 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render , get_object_or_404
-from .models import Book
-from .models import Library
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from .models import Book ,Library
 from django.views.generic.detail import DetailView
 
+
 # Class-Based View
-"relationship_app/library_detail.html"
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
@@ -14,3 +16,37 @@ class LibraryDetailView(DetailView):
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'list_books.html', {'books': books})
+# Create new user
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome {username}!')
+                return redirect('book_list')
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+#logout user
+def logout_view(request):
+    logout(request)
+    messages.info(request, 'You have been logged out.')
+    return redirect('login')
