f744da7f935283c1baca757830b0cba2c399ccdb
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-15 13:28:14 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 from api.models import Book
-from api.serializers import BookSerializer
-class BookList(generics.ListCreateAPIView):
+from .serializers import BookSerializer
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
