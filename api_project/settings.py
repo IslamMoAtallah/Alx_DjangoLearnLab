a9fe05fb59b205ae18b765eb27735b001095fd14
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-15 14:49:09 +0300

    Update settings.py

--- api_project/settings.py
+++ api_project/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',  # ✅
+        'rest_framework.authtoken',  # ✅
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  # الافتراضي: لازم تسجيل دخول
@@ -119,4 +119,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
