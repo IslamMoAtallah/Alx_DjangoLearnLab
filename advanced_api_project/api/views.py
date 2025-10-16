9a6252360691340fd4a7674e90959da736faec90
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-16 16:27:52 +0300

    Update views.py

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .models import MyModel
+from .serializers import MyModelSerializer
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]  # بس الادمن يقدر يعمل CRUD
+
+from rest_framework import generics
+from api.models import Book
+
 # Create your views here.
+
