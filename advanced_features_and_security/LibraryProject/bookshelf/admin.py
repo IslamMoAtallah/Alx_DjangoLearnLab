1ed6baaa36d136a74b516f20089c224b3a1c8ed4
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-17 19:21:40 +0300

    Implement CustomUserAdmin and UserAdmin in admin.py
    Added custom user admin for managing User and CustomUser models with additional fields.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,28 @@
 from .models import Book
 from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User,CustomUser  # Import the User model
+#CustomUser
+class CustomUserAdmin(BaseUserAdmin):
+    model= CustomUser
+    list_display = ['username', 'email', 'is_staff', 'is_active','date_of_birth']
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None,{'fields':('date_of_birth','profile_photo')}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None,{'fields':('date_of_birth','profile_photo')}),
+    )
+admin.site.register(CustomUser,CustomUserAdmin)
+# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Extra', {'fields': ('bio', 'profile_picture', 'followers')}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+
+admin.site.register(User, UserAdmin)
 admin.ModelAdmin
 list_filter = ('author', 'publication_year')
 search_fields = ('title', 'author')
@@ -8,3 +31,4 @@ admin.site.register(Book)
 
 
 # Register your models here.
+
