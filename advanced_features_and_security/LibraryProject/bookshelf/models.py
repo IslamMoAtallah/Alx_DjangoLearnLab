41b8c22babd194b6044c9aa0c423803b6c5d99cf
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-17 19:20:46 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,58 @@
 from django.db import models
-from django.apps import AppConfig 
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+# Custom user manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)   
+        return user
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+    # Custom user model
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    def __str__(self):
+        return self.username
+    
+def user_profile_picture_path(instance, filename):
+    return f'profiles/{instance.username}/{filename}'
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, default='')
+    profile_picture = models.ImageField(upload_to=user_profile_picture_path, null=True, blank=True)
+    
+    # followers: other users who follow this user
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
+
+    def follower_count(self):
+        return self.followers.count()
+
+    def following_count(self):
+        return self.following.count()
+
+    def __str__(self):
+        return self.username
+
 class BookshelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookshelf'
@@ -12,5 +65,7 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
 # Create your models here.
+
 
