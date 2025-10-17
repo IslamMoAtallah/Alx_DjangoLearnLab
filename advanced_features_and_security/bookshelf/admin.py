4dcc55cef37911d926299236b2b03f9067753b5d
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-17 19:08:13 +0300

    Add files via upload

---
+++ advanced_features_and_security/bookshelf/admin.py
@@ -0,0 +1,10 @@
+from .models import Book
+from django.contrib import admin
+admin.ModelAdmin
+list_filter = ('author', 'publication_year')
+search_fields = ('title', 'author')
+admin.site.register(Book)
+
+
+
+# Register your models here.
