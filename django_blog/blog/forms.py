5e27aa257d05e7c6a27cf3842139f4dfefeb69a0
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-18 19:44:45 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -17,4 +17,21 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
+
         fields = ['bio', 'avatar']
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        content = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Write your comment here...'}),
+        }
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Post title', 'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5 , 'placeholder': 'Write your post here...'}),
+        }
