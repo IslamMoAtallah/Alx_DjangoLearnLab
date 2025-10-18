fc109397506acdf4c743ef42480a4e654b45561d
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-18 19:22:13 +0300

    Fix URL pattern for post update path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,11 +10,12 @@ urlpatterns = [
     path('post/', PostListView.as_view(), name='post-list'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),  # for password reset and other
     
 ]
+
 
