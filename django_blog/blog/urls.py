c7ef9840dae6947cef6791a2d96f791e4b3870ea
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-18 19:03:52 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path, include   
+from django.urls import path, include
 from django.conf import settings
-from . import views
-from . views import CustomLoginView, CustomLogoutView
 from django.contrib.auth import views as auth_views
-urlpatterns = [
-    path('', views.register, name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('profile/', views.profile_view, name='profile'),
-     path('register/', views.register, name='register'),
-]
+from .views import (
+    PostListView, PostDetailView,
+    PostCreateView, PostUpdateView, PostDeleteView
+)
 
+urlpatterns = [
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),  # for password reset and other
+    
+]
