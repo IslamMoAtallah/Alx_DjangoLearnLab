7e3e231a63975065293dfd4678e269765b0b823a
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-19 20:47:10 +0300

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -10,11 +10,12 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return super().__str__()
-class comment(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(max_length=150)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
+
         return super().__str__()
