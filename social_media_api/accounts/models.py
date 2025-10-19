bd19119d90ecfc08bdf55b32957bb20b498ebe45
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-19 18:52:16 +0300

    Add files via upload

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from django.contrib.auth import get_user_model
+User = get_user_model()
+# Custom user manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)   
+        return user
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+    # Custom user model
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    def __str__(self):
+        return self.username
+    class Meta:
+        Permissions:[
+            ('can_view', 'Can view profile'),
+            ('can_edit', 'Can edit profile'),
+            ('can_create', 'Can create profile'),
+            ('can_delete', 'Can delete profile'),
+            ]
+    def __str__(self):
+        return self.username
+    
+def user_profile_picture_path(instance, filename):
+    return f'profiles/{instance.username}/{filename}'
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, default='')
+    profile_picture = models.ImageField(upload_to=user_profile_picture_path, null=True, blank=True)
+    
+    # followers: other users who follow this user
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
+
+    def follower_count(self):
+        return self.followers.count()
+
+    def following_count(self):
+        return self.following.count()
+
+    def __str__(self):
+        return self.username
+
+
+# Create your models here.
---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+user = get_user_model()
+class UserSerializer(serializers.ModelSerializer):
+    follower_count = serializers.IntegerField(source='follower_count', read_only=True)
+    following_count = serializers.IntegerField(source='following_count', read_only=True)
+    class Meta:
+        model = user
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'follower_count', 'following_count']
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,min_length=8)
+    Token = serializers.CharField(read_only=True)
+    class Meta:
+        model = user
+        fields = ['id', 'username', 'email', 'password']
+    def create(self, validated_data):
+        user = user.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        Token.objects.create(user=user)  # Create a token for the new user
+        return user
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
